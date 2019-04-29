b8fc58b2f06c38c29f5b3bce46d4f194fe4c33e1
Firat Atalay <firatatalay@gmail.com>
2019-04-29 17:02:17 +0300

    outreach activities added

--- src/app/components/outreach/outreach.component.ts
+++ src/app/components/outreach/outreach.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutreachComponent implements OnInit {
 
-  constructor() { }
+  outreach_1: string;
+  outreach_2: string;
+  outreach_3: string;
+  outreach_4: string;
+  outreach_5: string;
+  outreach_6: string;
+
+
+  constructor() { 
+    this.outreach_1 = '../../assets/images/outreach1.JPG'
+    this.outreach_2 = '../../assets/images/outreach2.JPG'
+    this.outreach_3 = '../../assets/images/outreach3.JPG'
+    this.outreach_4 = '../../assets/images/outreach4.JPG'
+    this.outreach_5 = '../../assets/images/outreach5.JPG'
+    this.outreach_6 = '../../assets/images/outreach6.JPG'
+  }
 
   ngOnInit() {
   }
